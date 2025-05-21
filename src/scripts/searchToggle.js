a13db014378b4de45f16c7d1f0878507e784caf3
Mounir Samite <mounir.samite@gmail.com>
2025-05-21 20:54:58 +0200

    clean repo from garbage, KISS

---
+++ src/scripts/searchToggle.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("search-toggle").addEventListener("click", function() {
+    document.getElementById("search-form").classList.toggle("hidden");
+  });
+});
