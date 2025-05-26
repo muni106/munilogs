546a664705c9e5b2bf2f9542d6d4b4a4025e054b
Mounir Samite <mounir.samite@gmail.com>
2025-05-26 14:12:00 +0200

    fix: change Readme and remove extra css

--- src/scripts/searchToggle.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  document.getElementById("search-toggle").addEventListener("click", function() {
-    document.getElementById("search-form").classList.toggle("hidden");
-  });
-});
