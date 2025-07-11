67587525d6b868fee4bc56bbc92c6ee91f3fcafd
Mounir Samite <mounir.samite@gmail.com>
2025-07-12 01:44:34 +0200

    feat: add post

--- src/utils/generateOgImages.ts
+++ src/utils/generateOgImages.ts
@@ -14,6 +14,11 @@ export async function generateOgImageForPost(post: CollectionEntry<"blog">) {
   return svgBufferToPngBuffer(svg);
 }
 
+export async function generateOgImageForProj(post: CollectionEntry<"projects">) {
+  const svg = await postOgImage(post);
+  return svgBufferToPngBuffer(svg);
+}
+
 export async function generateOgImageForSite() {
   const svg = await siteOgImage();
   return svgBufferToPngBuffer(svg);
